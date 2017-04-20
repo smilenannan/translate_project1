4115f58c2b923f1ae74168211d184d44489f5615
shoho <shoho.miura@gmail.com>
2017-04-20 16:44:13 +0900

    basic function

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,2 +1,17 @@
 class PapersController < ApplicationController
+  def index
+  end
+
+  def translate
+    @original = params.permit(:original)[:original]
+    @en_original = getTranslate(@original)
+    
+    @edit_en_original = params.permit(:edit_en_original)[:edit_en_original]
+    if params[:reflect]
+      @edit_en_original = @en_original
+    end
+    @ja_edit_en_original = getTranslate(@edit_en_original, 'en', 'ja')
+
+    render :action=>:index
+  end
 end
